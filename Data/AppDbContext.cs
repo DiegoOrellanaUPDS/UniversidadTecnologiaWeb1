a85c3ef6e93100025a093b5b9d6709baacb07cd8
lXBloodShootXl <lxbloodshootxl@gmail.com>
2026-01-26 08:27:37 -0400

    GitTest

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+namespace Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Entidades;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Estudiante> Estudiantes { get; set; }
+    }
+}
