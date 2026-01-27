d81bddb49557b3b2dce6d141aabaed4e655ef684
xfox <jbrayaa789@gmail.com>
2026-01-27 09:01:06 -0400

    Prueba anadi entidad departamento

--- Entidades/Departamento.cs
+++ Entidades/Departamento.cs
@@ -4,13 +4,15 @@ namespace Entidades
 {
     public class Departamento
     {
-        public int Id { get; set; }
+        public int Id{ get; set; }
         public string Nombre { get; set; } = null!;
         public string? Descripcion { get; set; }
         public string Responsable { get; set; } = null!;
         public string? Telefono { get; set; }
         public string? Email { get; set; }
 
+        
+        
         public bool Estado { get; set; }
     }
 
